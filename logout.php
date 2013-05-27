6cac2776b9850eff43accc1c55b77ab42cbb5911
R Yosi Nurrahman <r.masyosi@gmail.com>
2013-05-27 23:57:45 +0700

    Revert "Tugas Prokus"
    This reverts commit 7edc775135e8d030de215748a25fd3d631480572.

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(0);
-session_start();
-session_destroy();
-session_unset($_SESSION['username'], $_SESSION['password']);
-echo "<script language='javascript'>document.location.href='index.php';</script>";
-?>
